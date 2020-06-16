263e49aad00581bc0349bf7d612e71b4e4dd0229
Elvin Peng <elvinpeng@tencent.com>
2020-06-16 19:49:44 +0800

    feat: 03_basic_io

---
+++ 03_basic_io/basic_io.cpp
@@ -0,0 +1,28 @@
+// Compile: clang++ -std=c++2a -stdlib=libc++ -Wall -O3 basic_io.cpp -o basic_io.out
+// -Wall: Warnings
+// -O3: Optimization level - Maximum
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+  string name;
+  string str;
+  int age;
+
+  cout << "What is your name?\n";
+
+  // use getline to avoid space split
+  getline(cin, name);
+
+  cout << "What is your age?\n";
+  getline(cin, str);
+  // use stringstream to conver string to number
+  stringstream(str) >> age;
+
+  cout << "Hello " << name << ". Your age is " <<  age << ".\n";
+  return 0;
+}
